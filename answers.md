f22681eb1f0646c72ece10bc58949a11362ee09e
Andrew Taylor <agrahamt@gmail.com>
2017-04-04 13:20:27 -0500

    added answers.md and started exercises.js, stuck on while add/remove question

---
+++ answers.md
@@ -0,0 +1,27 @@
+1) What is the index of the first item in an array?
+**0**
+1
+i
+.first()
+
+2) Array literals are denoted by:
+**An opening square bracket, a comma separated list of data, and a closing square bracket.**
+A opening curly brace, a comma separated list of data, and a closing curly brace
+A opening curly brace, a colon separated list of data, and a closing curly brace
+A pair of double quotation marks followed by a semicolon.
+
+3) Which of the following are examples of accessing a particular element from an array named groceries? (Select all that apply)
+groceries[2] = "Broccoli";
+**var item = groceries[2];**
+var groceries = ["Milk", "Chicken", "Noodles"];
+var item = groceries.get(2);
+4) What does the Array object's .unshift() method do?
+Adds an item to the end of the array
+**Adds an item to the beginning of the array**
+Removes and returns the last item in an array
+Removes and returns the first item in an array
+5) Which method of Array can be used to iterate over the elements contained in the Array?
+**for()**
+each()
+foreach()
+forEach()
---
+++ exercises.js
@@ -0,0 +1,29 @@
+// In your own words, write a definition of arrays.
+// An arrays is are collections of things.
+
+// Write a short program in which you create a grocery list containing 10 items using bracket notation (not array literals). Output the grocery list to the console as a whole. Output only the first and then the 5th elements in the array to the console.
+var groceryList = ['cookies', 'cake', 'pomegranate', 'bread', 'milk', 'salsa', 'corn chips', 'orange juice', 'yogurt', 'candy']
+grocer
+console.log(groceryList)
+console.log(groceryList[5], groceryList[1])
+// Write code that does the following:
+//
+// Create an array of 10 arbitrary numbers using array literal syntax.
+var firstArray = [3, 6, 8, 4, 7, 3, 4, 8, 4, 4]
+// Next, create a second empty array.
+var secondArray = []
+// Using a while loop, write code that removes data from the first array and places it into the second array.
+// https://goo.gl/w7GqGk
+
+// At the end of this process the first array should be empty and the second array should contain the same numbers, but in reverse order.
+// You are given the following array of objects:
+//
+// Each object represents a person with a first and last name. Your job is to print a numbered list of the initials of each person. Use forEach() to iterate over the array.
+//
+// The expected output is:
+//
+// 1) L. X.
+// 2) M. D.
+// 3) M. B.
+// 4) H. F.
+// 5) B. A.
